8cbc3f6b3989a81242462cae111c5e6400cb5034
variadico <variadico@users.noreply.github.com>
2015-04-29 17:37:54 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# scaneo
+Generate code to scan SQL queries into structs.
+
+This doesn't work yet. Tee hee. :P
---
+++ tmpl.go
@@ -0,0 +1,37 @@
+package main
+
+const (
+	scanStructFunc = `{{range .}}func Scan{{.StructName}}(r *sql.Row) ({{.StructName}}, error) {
+	var s {{.StructName}}
+
+	if err := r.Scan({{range .FieldName}}
+		&s.{{.}},{{end}}
+	); err != nil {
+		return {{.StructName}}{}, err
+	}
+
+	return s, nil
+}
+
+{{end}}`
+
+	scanStructsFunc = `{{range .}}func Scan{{.StructName}}s(rs *sql.Rows) ([]{{.StructName}}, error) {
+	structs := make([]{{.StructName}}, 0, 16)
+
+	var err error
+	for rs.Next() {
+		var s {{.StructName}}
+
+		if err = rs.Scan({{range .FieldName}}
+			&s.{{.}},{{end}}
+		); err != nil {
+			return nil, err
+		}
+
+		structs = append(structs, s)
+	}
+
+	return structs, nil
+}
+{{end}}`
+)
