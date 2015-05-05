3c75bb31c9173b1df799fa9f1433d5de782d7027
variadico <variadico@users.noreply.github.com>
2015-05-05 01:08:10 -0700

    rename struct info fields

--- test.go
+++ test.go
@@ -12,14 +12,14 @@ const (
 	srcFile = "testdata/structs.go"
 )
 
-type structData struct {
-	Name  string
-	Field []string
-	Type  []string
+type structToken struct {
+	Name   string
+	Fields []string
+	Types  []string
 }
 
 func main() {
-	sdatas := make([]structData, 0, 8)
+	sdatas := make([]structToken, 0, 8)
 
 	fset := token.NewFileSet()
 	astf, err := parser.ParseFile(fset, srcFile, nil, 0)
@@ -30,9 +30,9 @@ func main() {
 
 	// ast.Print(fset, astf)
 	for _, dec := range astf.Decls {
-		sdata := structData{
-			Field: make([]string, 0, 8),
-			Type:  make([]string, 0, 8),
+		sdata := structToken{
+			Fields: make([]string, 0, 8),
+			Types:  make([]string, 0, 8),
 		}
 
 		genDec, isGenDec := dec.(*ast.GenDecl)
@@ -55,19 +55,19 @@ func main() {
 
 			for _, field := range structType.Fields.List {
 				for _, ident := range field.Names {
-					sdata.Field = append(sdata.Field, ident.Name)
+					sdata.Fields = append(sdata.Fields, ident.Name)
 				}
 
 				switch fieldType := field.Type.(type) {
 				case *ast.Ident:
-					sdata.Type = append(sdata.Type, fieldType.Name)
+					sdata.Types = append(sdata.Types, fieldType.Name)
 				case *ast.SelectorExpr:
 					ident, isIdent := fieldType.X.(*ast.Ident)
 					if !isIdent {
 						continue
 					}
 
-					sdata.Type = append(sdata.Type,
+					sdata.Types = append(sdata.Types,
 						fmt.Sprintf("%s.%s", ident.Name, fieldType.Sel.Name))
 				}
 
@@ -80,13 +80,13 @@ func main() {
 	for _, sd := range sdatas {
 		fmt.Println(sd.Name)
 
-		if len(sd.Field) != len(sd.Type) {
+		if len(sd.Fields) != len(sd.Types) {
 			log.Println(sd)
 			log.Fatal("Ahhh!!")
 		}
 
-		for i, _ := range sd.Field {
-			fmt.Println("   ", sd.Field[i], sd.Type[i])
+		for i, _ := range sd.Fields {
+			fmt.Println("   ", sd.Fields[i], sd.Types[i])
 		}
 	}
 }
