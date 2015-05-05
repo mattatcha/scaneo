1de0773abf7813c49e7e79f69f3eb686b28b0930
variadico <variadico@users.noreply.github.com>
2015-05-05 01:45:25 -0700

    add structtok string func

--- test.go
+++ test.go
@@ -1,15 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
-)
-
-const (
-	srcFile = "testdata/structs.go"
 )
 
 type structToken struct {
@@ -18,14 +15,47 @@ type structToken struct {
 	Types  []string
 }
 
+func (tok structToken) String() string {
+	if len(tok.Fields) != len(tok.Types) {
+		log.Println("len(tok.Fields) != len(tok.Types)")
+		log.Println("something went wrong with the parsing...")
+		log.Println("continuing anyway")
+	}
+
+	var buf bytes.Buffer
+	buf.WriteString(tok.Name)
+	buf.WriteString("\n")
+
+	for i, _ := range tok.Fields {
+		buf.WriteString("    ")
+		buf.WriteString(tok.Fields[i])
+		buf.WriteString(" ")
+		buf.WriteString(tok.Types[i])
+		buf.WriteString("\n")
+	}
+
+	return buf.String()
+}
+
 func main() {
+	toks, err := parseCode("testdata/structs.go")
+	if err != nil {
+		log.Println(`"syntax error" - parser probably`)
+		log.Fatal(err)
+	}
+
+	for _, t := range toks {
+		fmt.Println(t)
+	}
+}
+
+func parseCode(srcFile string) ([]structToken, error) {
 	structToks := make([]structToken, 0, 8)
 
 	fset := token.NewFileSet()
 	astf, err := parser.ParseFile(fset, srcFile, nil, 0)
 	if err != nil {
-		log.Println(`"syntax error" - parser probably`)
-		log.Fatal(err)
+		return nil, err
 	}
 
 	// ast.Print(fset, astf)
@@ -68,7 +98,7 @@ func main() {
 					}
 
 					structTok.Types = append(structTok.Types,
-						fmt.Sprintf("%s.%s", ident.Name, fieldType.Sel.Name))
+						fmt.Sprint(ident.Name, ".", fieldType.Sel.Name))
 				}
 
 			}
@@ -77,16 +107,5 @@ func main() {
 		}
 	}
 
-	for _, sd := range structToks {
-		fmt.Println(sd.Name)
-
-		if len(sd.Fields) != len(sd.Types) {
-			log.Println(sd)
-			log.Fatal("Ahhh!!")
-		}
-
-		for i, _ := range sd.Fields {
-			fmt.Println("   ", sd.Fields[i], sd.Types[i])
-		}
-	}
+	return structToks, nil
 }
