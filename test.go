6e886ab9b81a948aca522fcd7a31011a7f29c9f9
variadico <variadico@users.noreply.github.com>
2015-05-04 23:43:47 -0700

    flatten out code; FIX extra append

--- test.go
+++ test.go
@@ -28,51 +28,54 @@ func main() {
 		log.Fatal(err)
 	}
 
-	ast.Print(fset, astf)
+	// ast.Print(fset, astf)
 	for _, dec := range astf.Decls {
 		sdata := structData{
 			Field: make([]string, 0, 8),
 			Type:  make([]string, 0, 8),
 		}
 
-		switch decType := dec.(type) {
-		case *ast.GenDecl:
-			for _, spec := range decType.Specs {
-				switch sp := spec.(type) {
-				case *ast.TypeSpec:
-					sdata.Name = sp.Name.Name
+		decType, isGenDec := dec.(*ast.GenDecl)
+		if !isGenDec {
+			continue
+		}
 
-					// Type: *ast.StructType
-					st, isStructType := sp.Type.(*ast.StructType)
-					if !isStructType {
+		for _, spec := range decType.Specs {
+			sp, isTypeSpec := spec.(*ast.TypeSpec)
+			if !isTypeSpec {
+				continue
+			}
+
+			sdata.Name = sp.Name.Name
+
+			st, isStructType := sp.Type.(*ast.StructType)
+			if !isStructType {
+				continue
+			}
+
+			//  List: []*ast.Field (len = 2)
+			for _, fl := range st.Fields.List {
+				for _, ident := range fl.Names {
+					sdata.Field = append(sdata.Field, ident.Name)
+				}
+
+				switch tp := fl.Type.(type) {
+				case *ast.Ident:
+					sdata.Type = append(sdata.Type, tp.Name)
+				case *ast.SelectorExpr:
+					ident, isIdent := tp.X.(*ast.Ident)
+					if !isIdent {
 						continue
 					}
 
-					//  List: []*ast.Field (len = 2)
-					for _, fl := range st.Fields.List {
-						for _, ident := range fl.Names {
-							sdata.Field = append(sdata.Field, ident.Name)
-						}
-
-						switch tp := fl.Type.(type) {
-						case *ast.Ident:
-							sdata.Type = append(sdata.Type, tp.Name)
-						case *ast.SelectorExpr:
-							ident, isIdent := tp.X.(*ast.Ident)
-							if !isIdent {
-								continue
-							}
-
-							sdata.Type = append(sdata.Type,
-								fmt.Sprintf("%s.%s", ident.Name, tp.Sel.Name))
-						}
-
-					}
+					sdata.Type = append(sdata.Type,
+						fmt.Sprintf("%s.%s", ident.Name, tp.Sel.Name))
 				}
-			}
-		}
 
-		sdatas = append(sdatas, sdata)
+			}
+
+			sdatas = append(sdatas, sdata)
+		}
 	}
 
 	for _, sd := range sdatas {
